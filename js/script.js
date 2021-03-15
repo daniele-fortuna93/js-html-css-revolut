f3db342222f18712a8ccf1d00fbf99057be5966e
Daniele <bambudio@libero.it>
2021-03-15 18:04:53 +0100

    Aggiunto evento click e hover js

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('.main_nav_li').click(
+    function () {
+      if ( !$(this).children('.dropdown').hasClass('activeClick') ) {
+        $('.dropdown').removeClass('activeClick');
+        $(this).children('.dropdown').addClass('activeClick');
+      } else {
+        $(this).children('.dropdown').removeClass('activeClick');
+        $(this).children('.dropdown').removeClass('active');
+      }
+    }
+  );
+  $('.main_nav_li').hover(
+    function(){
+      $('.dropdown').removeClass('activeClick');
+      $(this).children('.dropdown').addClass('active');
+    },
+    function(){
+      $(this).children('.dropdown').removeClass('active');
+    }
+  );
+
+  }
+);
